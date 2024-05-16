33fbe5f0492f32d5f7d39cd390dd83eed28bce7c
Kevin Dubois <kevin.dubois@gmail.com>
2024-05-16 14:22:21 +0200

    remove index page

--- src/main/java/org/acme/InstructLabResource.java
+++ src/main/java/org/acme/InstructLabResource.java
@@ -4,10 +4,9 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/instructlab")
+@Path("/")
 public class InstructLabResource {
 
     @Inject
