427554d6d9b062a15e3176a5bf5d0aec10212b7c
Kevin Dubois <kevin.dubois@gmail.com>
2024-05-16 13:41:24 +0200

    initial commit

---
+++ src/main/java/org/acme/AssistantForInstructLab.java
@@ -0,0 +1,16 @@
+package org.acme;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.enterprise.context.SessionScoped;
+
+@RegisterAiService()
+@SessionScoped
+public interface AssistantForInstructLab {
+
+    @SystemMessage({
+            "You are a Java developer who likes to over engineer things"
+    })
+    String chat(@UserMessage String userMessage);
+}
---
+++ src/main/java/org/acme/InstructLabResource.java
@@ -0,0 +1,23 @@
+package org.acme;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/instructlab")
+public class InstructLabResource {
+
+    @Inject
+    AssistantForInstructLab assistant;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String prompt() {
+        // feel free to update this message to any question you may have for the LLM.
+        String message = "Generate a class that returns the square root of a given number";
+        return assistant.chat(message);
+    }
+}
