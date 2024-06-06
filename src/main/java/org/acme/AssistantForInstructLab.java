26a173b7ceec7176b43eb1476a397126146ec093
Kevin Dubois <kevin.dubois@gmail.com>
2024-06-06 17:40:59 +0200

    Update Quarkus & LangChain4j extension versions

--- src/main/java/org/acme/AssistantForInstructLab.java
+++ src/main/java/org/acme/AssistantForInstructLab.java
@@ -6,7 +6,6 @@ import io.quarkiverse.langchain4j.RegisterAiService;
 import jakarta.enterprise.context.SessionScoped;
 
 @RegisterAiService()
-@SessionScoped
 public interface AssistantForInstructLab {
 
     @SystemMessage({
