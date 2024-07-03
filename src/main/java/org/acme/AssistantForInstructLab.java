8074c45d22c88620cc65b65267011652024e532f
Kevin Dubois <kevin.dubois@gmail.com>
2024-07-03 18:56:14 +0200

    Add FallbackHandler to AI Assistant

--- src/main/java/org/acme/AssistantForInstructLab.java
+++ src/main/java/org/acme/AssistantForInstructLab.java
@@ -1,9 +1,13 @@
 package org.acme;
 
+import org.eclipse.microprofile.faulttolerance.ExecutionContext;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.FallbackHandler;
+import org.eclipse.microprofile.faulttolerance.Retry;
+
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
-import jakarta.enterprise.context.SessionScoped;
 
 @RegisterAiService()
 public interface AssistantForInstructLab {
@@ -11,5 +15,19 @@ public interface AssistantForInstructLab {
     @SystemMessage({
             "You are a Java developer who likes to over engineer things"
     })
+    @Retry(maxRetries = 3, delay = 100)
+    @Fallback(AssistantForInstructLabFallback.class)
     String chat(@UserMessage String userMessage);
+
+    public static class AssistantForInstructLabFallback implements FallbackHandler<String> {
+
+        private static final String EMPTY_RESPONSE = "Failed to get a response from the AI Model. Are you sure it's up and running, and configured correctly?";
+        @Override
+        public String handle(ExecutionContext context) {
+            return EMPTY_RESPONSE;
+        }
+    
+    }
 }
+
+
